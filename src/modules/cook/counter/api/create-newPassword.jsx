6eef4a6408bf48f8ce8a6e58564cff0345e7ddfc
Alakaraitai <alaka.rai@tai.com.np>
2025-03-05 15:12:40 +0545

    login page added

--- src/modules/cook/counter/api/create-newPassword.jsx
+++ src/modules/cook/counter/api/create-newPassword.jsx
@@ -1,10 +1,4 @@
 export const CreateNewPassword = (data) => {
-  return api.post(`/admin/reset-password`, data);
-};
-// export const useCreateNewPassword=({
-//     mutationConfig,
-// }:useCreateNewPassword)
-export const CreateNewPassword = (data) => {
   return api.post(`/cook/reset-password`, data);
 };
 // export const useCreateNewPassword=({
