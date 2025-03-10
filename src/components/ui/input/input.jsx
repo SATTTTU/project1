7ba2a688984260af2471e1078eeb8fc6518028f2
pukar.khanal <pukar.khanal@tai.com.np>
2025-03-10 09:08:07 +0000

    path fixes

---
+++ src/components/ui/input/input.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Input = ({ type = "text", placeholder, value, onChange, className, id, name }) => {
+  return (
+    <input
+      id={id}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className={`border rounded-md px-3 py-2 focus:ring-1 focus:ring-blue-500 focus:outline-none ${className}`}
+    />
+  );
+};
+
+export default Input;
