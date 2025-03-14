e8067e964a89811cb1b10062d44f260a45cd5bf1
indralimbutai <indrabahadur.limbu@tai.com.np>
2025-03-14 18:08:50 +0545

    feat: integrate register api

--- src/modules/user/auth/components/userLogin.jsx
+++ src/modules/user/auth/components/userLogin.jsx
@@ -108,12 +108,12 @@ export const LoginSection = () => {
 				<img src={Image} alt="Welcome" className="object-cover w-full h-full" />
 				<div className="absolute inset-0 bg-black/40 flex flex-col items-center justify-center text-white p-6">
 					<h2 className="text-4xl font-bold mb-4">Hello, Friends</h2>
-					<p className="text-center text-2xl mb-2">Enter your Personal Details</p>
-					<p className="text-center text-2xl mb-4">Start journey with us</p>
+					<p className="text-center text-xl mb-2">Enter your Personal Details</p>
+					<p className="text-center text-xl mb-4">Start journey with us</p>
 
 					<Link
 						to="/user/register"
-						className="bg-white text-black px-6 py-3 text-xl rounded hover:bg-[#426B1F] transition"
+						className="bg-white text-black px-6 py-3 text-xl rounded hover:text-white hover:bg-[#426B1F] transition"
 					>
 						Sign Up
 					</Link>
