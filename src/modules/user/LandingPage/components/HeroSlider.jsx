e022f68db8bbcba939031e98e0f709fc85bc5d23
alakaraitai <alaka.rai@tai.com.np>
2025-03-14 04:28:35 +0000

    changes in components

--- src/modules/user/LandingPage/components/HeroSlider.jsx
+++ src/modules/user/LandingPage/components/HeroSlider.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { Header } from "./header"
+import { Header } from "./Header"
 import MainImage from "../../../../assets/Main.jpg"
 
 
