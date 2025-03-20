d9a363fc0583b81b92eef16eab0f6c3bc7e6f619
alakaraitai <alaka.rai@tai.com.np>
2025-03-20 03:28:03 +0000

    canages in file export

--- src/modules/cook/homepage/component/ordermanagement.jsx
+++ src/modules/cook/homepage/component/ordermanagement.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import OrderRequest from "./OrderRequest";
-import ActiveOrder from "./ActiveOrder";
+import OrderRequest from "./orderrequest";
+import ActiveOrder from "./activeorder";
+
 
 const OrderManagement = ({ orderRequests, activeOrders }) => {
   return (
