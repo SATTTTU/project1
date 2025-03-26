d3c9f68c70e4e6b844ca6beb09478caeb34fd7f5
Takhangma Rai <takhangma.rai@tai.com.np>
2025-03-26 14:20:46 +0545

    Safe commit

---
+++ src/modules/cook/earnings/api/withdrawEarningRequest.jsx
@@ -0,0 +1,24 @@
+import { api } from "@/lib/api-client";
+import { useMutation } from "@tanstack/react-query";
+
+// Fixing async and await
+// eslint-disable-next-line react-refresh/only-export-components
+const WithdrawEarningsRequest = async (cookdata) => {
+  const response = await api.post("/api/cooks/withdraw-request", cookdata);
+  return response.data; // Return response data
+};
+
+export const useWithdrawEarningsRequest = ({ mutationConfig } = {}) => {
+  const mutation = useMutation({
+    mutationFn: WithdrawEarningsRequest,
+    ...mutationConfig,
+  });
+
+  return {
+    mutateAsync: mutation.mutateAsync,
+    isLoading: mutation.isLoading, // Fixed the state
+    error: mutation.error,
+    isError: mutation.isError,
+    isSuccess: mutation.isSuccess,
+  };
+};
